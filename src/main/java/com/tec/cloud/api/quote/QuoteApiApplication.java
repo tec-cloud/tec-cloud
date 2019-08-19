a7f70f5d83729ec8688635712c78434da0362401
Candyメ奶糖 <359031282@qq.com>
2019-08-19 15:57:36 +0800

    Upgrade architecture

--- src/main/java/com/tec/cloud/api/quote/QuoteApiApplication.java
+++ src/main/java/com/tec/cloud/api/quote/QuoteApiApplication.java
@@ -2,15 +2,12 @@ package com.tec.cloud.api.quote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author tec
  */
-@EnableEurekaClient
 @SpringBootApplication
-@ComponentScan({"com.tec.platform.config", "com.tec.cloud.api.quote"})
+//@ComponentScan({"com.tec.platform.config", "com.tec.cloud.api.quote"})
 public class QuoteApiApplication {
 
     public static void main(String[] args) {
